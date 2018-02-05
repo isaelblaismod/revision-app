83d5f82eb6de6f41a91e21db9fadf4fd68c0926f
isaelblaismod <isael.blais@modellium.com>
2018-02-04 22:37:50 -0500

    Route defines & adding bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 
+/* Custom component */
+import { PeopleComponentComponent } from './people-component/people-component.component'
+import { HomeComponentComponent } from './home-component/home-component.component';
+import { PageNotFoundComponentComponent } from './page-not-found-component/page-not-found-component.component';
+
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponentComponent },
+  { path: 'people', component: PeopleComponentComponent },
+  { path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  { path: '**', component: PageNotFoundComponentComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponentComponent,
+    PeopleComponentComponent,
+    PageNotFoundComponentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
   ],
   providers: [],
   bootstrap: [AppComponent]
